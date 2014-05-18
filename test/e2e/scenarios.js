b45d15d64c9719a80dc7a8b1540508024a09d3df
Edwin Knuth <eknuth@gmail.com>
2014-05-17 22:36:24 -0700

    e2e tests for sortable behavior for #2

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -18,11 +18,38 @@ describe('my app', function() {
         });
 
 
-        it('should have some items', function() {
+        it('should have some items, sorted by score (desc) or name', function() {
             var itemNames = [
                 'Item C', 'Item B', 'Item A'
             ];
-            var itemScores = [3, 2, 1]
+            var itemScores = [3, 2, 1];
+
+            var itemNamesSorted = [
+                'Item A', 'Item B', 'Item C'
+            ];
+            var itemScoresSorted = [1, 2, 3];
+            var sortByScoreBtn = element(by.css('.by-score'));
+            var sortByNameBtn = element(by.css('.by-name'));
+
+            // sort by score is the default
+            element.all(by.repeater('item in items')).then(function(rows) {
+                rows.forEach(function(row, index) {
+                    expect(row.getText()).toContain(itemNames[index]);
+                    expect(row.getText()).toContain(itemScores[index]);
+                });
+            });
+
+            // sort by score is the default
+            sortByNameBtn.click();
+            element.all(by.repeater('item in items')).then(function(rows) {
+                rows.forEach(function(row, index) {
+                    expect(row.getText()).toContain(itemNamesSorted[index]);
+                    expect(row.getText()).toContain(itemScoresSorted[index]);
+                });
+            });
+
+            // switch back
+            sortByScoreBtn.click();
             element.all(by.repeater('item in items')).then(function(rows) {
                 rows.forEach(function(row, index) {
                     expect(row.getText()).toContain(itemNames[index]);
